892bac4ec0838053d271804382ef27378d418cae
Patrick Wiseman <patrick@flowerwork.io>
2024-02-22 20:39:39 -0500

    Remove some more linger mongo adapter pieces

--- repl.py
+++ repl.py
@@ -5,10 +5,8 @@ import feature_gate.clients
 import feature_gate.clients.posthog_api_client
 import feature_gate.feature
 import feature_gate.adapters
-import feature_gate.adapters.mongo
 import feature_gate.adapters.posthog
 
-from feature_gate.adapters.mongo import MongoAdapter
 from feature_gate.adapters.posthog import PosthogAdapter
 from feature_gate.client import Client
 from feature_gate.clients.posthog_api_client import PosthogAPIClient
@@ -18,7 +16,3 @@ def posthog_client() -> Client:
   adapter = PosthogAdapter()
   return Client(adapter)
 
-def mongo_client() -> Client:
-  db = MongoClient('mongodb://feature_gate:feature_gate@localhost:27017/feature_gate')
-  adapter = MongoAdapter(db)
-  return Client(adapter)
